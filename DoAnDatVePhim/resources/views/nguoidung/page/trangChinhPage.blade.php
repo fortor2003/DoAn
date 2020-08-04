438e8be359df2011eaf1962aee6031d4f11fe2d3
fortor2003 <44230272+fortor2003@users.noreply.github.com>
2020-08-04 15:26:57 +0700

    32604062020

--- DoAnDatVePhim/resources/views/nguoidung/page/trangChinhPage.blade.php
+++ DoAnDatVePhim/resources/views/nguoidung/page/trangChinhPage.blade.php
@@ -1,2 +1,74 @@
 @extends('nguoidung.layout.master')
 
+@section('noiDung')
+{{-- tab --}}
+<ul class="nav nav-tabs">
+    <li class="nav-item">
+        <a class="nav-link active" href="#tabDangChieu">Phim đang chiếu</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#tabSapChieu">Phim sắp chiếu</a>
+    </li>
+</ul>
+{{-- nội dung --}}
+<div class="tab-content"> 
+    <div class="tab-pane container active" id="tabDangChieu">
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+        </div>
+    </div>
+    <div class="tab-pane container fade" id="tabSapChieu"> <p>Thử thay đổi gì đó khi chuyển tab.</p></div> 
+</div>
+@endsection
+

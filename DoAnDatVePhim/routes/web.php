9036bd8e9e6cc9a495be62de7241a3eb60da1ab0
Nguyễn Hoàng Thanh Long <nguyenhoangthanhlong1996@gmail.com>
2020-08-08 17:28:35 +0700

    asdasdasd

--- DoAnDatVePhim/routes/web.php
+++ DoAnDatVePhim/routes/web.php
@@ -3,17 +3,3 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/aaaa', 'khachHang\PageController@trangChu');
-////////////Người dùng
-//Trang chủ
-Route::get('/', function (){
-    return view('nguoidung.page.trangChinhPage');
-});
-//đăng nhập
-Route::get('/dang-nhap', function (){
-    return view('nguoidung.page.dangNhapPage');
-});
-//Chi tiết phim
-Route::get('/dang-nhap', function (){
-    return view('nguoidung.page.chiTietPhimPage');
-});
